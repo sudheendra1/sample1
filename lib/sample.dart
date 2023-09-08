0f14fe3b1f067c8ff6e53dfe681ed330a3fc049a
sudhe <sudheendra579@gmail.com>
2023-09-08 06:33:30 +0530

    api

--- lib/sample.dart
+++ lib/sample.dart
@@ -1,6 +1,9 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:sample/blank.dart';
+import 'package:sample/function.dart';
 
 class Sample extends StatefulWidget{
   const Sample({super.key});
@@ -14,14 +17,34 @@ class Sample extends StatefulWidget{
 
 class _blankstate extends State<Sample>{
   final text = TextEditingController();
+  String url='';
+  var data;
+  String output='initial';
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(
       child: Column(mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(width: 350,child: TextField(controller: text,decoration: InputDecoration(label: Text('Query')),)),
+          SizedBox(width: 350,child: TextField(onChanged: (value){
+            url='http://punio.serveo.net/api?query='+value.toString();
+          },decoration: InputDecoration(label: Text('Query')),)),
           SizedBox(height: 20,),
-          ElevatedButton(onPressed: (){print(text.text);}, child: Text('Submit')),
+          ElevatedButton(onPressed: ()async{data= await fetchdata(url);
+          var decoded;
+if (data!=null&&data.isNotEmpty){
+  decoded = jsonDecode(data);
+}else {
+  decoded='fuck u';
+}
+
+            setState(() {
+              output=decoded['output'];
+              print(output);
+            });
+            }, child: Text('Submit')),SizedBox(height: 20,),
+          Text(output),
+
+
           ElevatedButton(onPressed: (){Navigator.push(context , MaterialPageRoute(builder: (context)=>Blank()));}, child: Text('Blank screen'))
         ],
       ),
